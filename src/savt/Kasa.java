6e11492cb16bd77861a6e88197ff55a92dcca43d
eneserkoc <eneserkoc@github.com>
2017-05-07 22:17:55 +0300

    class step8

--- src/savt/Kasa.java
+++ src/savt/Kasa.java
@@ -1,17 +1,17 @@
 package savt;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.io.IOException;
+import java.util.*;
 /**
  * Created by enes on 07.05.2017.
  */
 public class Kasa {
 
-
+    ArrayList<Urun> alisveris = new ArrayList<Urun>();
     private double gunSonuSatisTutari = 0;
     private int gunlukMusteri = 0;
-
+    //TODO : IOException da olabilir
+    //urun un bulunamadigi dusunulerek  nullPointerException verildi
 
 
     public int getGunlukMusteri() {
@@ -40,45 +40,20 @@ public class Kasa {
         gunSonuSatisTutari += satisTutari;
     }
 
-<<<<<<< HEAD
-    public boolean urunVarMi(HashMap<String, Urun> urunler, String isim)
+    public Urun barkodAra(ArrayList<Urun> urunler, String barkod) throws NullPointerException
     {
-        for( HashMap.Entry<String, Urun> entry : urunler.entrySet() )
+        for(Urun urun : urunler)
         {
-            if(entry.getValue().getIsim().compareTo(isim) == 0)
+            if((urun.getBarkod()).compareTo(barkod) == 0)
             {
-                System.out.println("Aranan urun mevcut . Stok miktari : "+ entry.getValue().getStok());
-                return true;
+                return urun;
             }
         }
-        //okuyucu.close();
-        System.out.println("Aranan urun bulunamadi...\n");
-        return false;
-
-    }
-    //TODO : IOException da olabilir
-    //urun un bulunamadigi dusunulerek  nullPointerException verildi
-<<<<<<< HEAD
-    public Urun barkodAra(HashMap<String, Urun> urunler, String barkod) throws NullPointerException
-=======
-    public Urun barkodAra(/*HashMap<Integer, String>*/ArrayList<Urun> urunler, String barkod) throws NullPointerException
->>>>>>> c45036d... hashmap yapısı sonere çekildi
-=======
-    public Urun barkodAra(HashMap<String, Urun> urunler, String barkod)
->>>>>>> b1ef317... fire mire
-    {
-        for( HashMap.Entry<String, Urun> entry : urunler.entrySet() )
-        {
-            if(entry.getValue().getBarkod().compareTo(barkod) == 0)
-            {
-                return (entry.getValue());
-            }
-        }
-        return null;
+        throw new NullPointerException("Girilen barkoda sahip bir urun bulunamadi...\n");
 
     }
 
-    public double toplamFiyatHesapla(ArrayList<Urun> alisveris)
+    public double toplamFiyatHesapla()
     {
         double toplamFiyat = 0;
 
@@ -89,9 +64,9 @@ public class Kasa {
 
         return toplamFiyat;
     }
-    public void Satis(HashMap<String, Urun> urunler)
+    public void Satis(ArrayList<Urun> urunler) throws IOException
     {
-        ArrayList<Urun> alisveris = new ArrayList<Urun>();
+
         boolean exit = false;
 
         Scanner in = new Scanner(System.in);
@@ -108,33 +83,24 @@ public class Kasa {
 =======
             System.out.println(" Seciminizi giriniz : ");
             int selection = in.nextInt();
-            in.nextLine();
             switch(selection)
 >>>>>>> 8101031... class step4
             {
-                case 1 : //TODO : Urun urun yukarida da tanimlanabilir.Emin degiliz
-                        try{
-                            System.out.println("Alinmak istenen urunun barkodunu giriniz : ");
-                            String barkod = in.nextLine();
-                             Urun urun = barkodAra(urunler, barkod);
-                            System.out.println("Urun sayisini giriniz : ");
-                            int adet = in.nextInt();
-                             if(urun.stoktanDus(adet))
-                            {
-                                for(int i=0; i<adet; i++)
-                                {
-                                    alisveris.add(urun);
-                                }
-                            }
-                        }
-                        catch(NullPointerException e)
-                        {
-                            System.out.println("Girilen barkoda sahip bir urun bulunamadi");
-                        }
+                case 1 : System.out.println("Alinmak istenen urunun barkodunu giriniz : ");
+                         String barkod = in.nextLine();
+                         System.out.println("Urun sayisini giriniz : ");
+                         int adet = in.nextInt();
+                         //TODO : Urun urun yukarida da tanimlanabilir.Emin degiliz
+                         Urun urun = barkodAra(urunler, barkod);
+                         for(int i=0; i<adet; i++)
+                         {
+                             alisveris.add(urun);
+                         }
+                         urun.stoktanDus(adet);
                 break;
-                case 2 : gunSonunaEkle(toplamFiyatHesapla(alisveris));
+                case 2 : gunSonunaEkle(toplamFiyatHesapla());
                          gunlukMusteriArttir();
-                         System.out.println("Toplam alisveris tutari : "+ toplamFiyatHesapla(alisveris));
+                         System.out.println("Toplam alisveris tutari : "+ toplamFiyatHesapla());
                          System.out.println("Alisverisi bitti...Iyi gunler dileriz \n");
                          exit = true;
                 break;
