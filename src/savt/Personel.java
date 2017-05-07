14f75350f30c01f427e794f119495e852f75a39e
eneserkoc <eneserkoc@github.com>
2017-05-07 19:14:46 +0300

    class step1

--- src/savt/Personel.java
+++ src/savt/Personel.java
@@ -3,8 +3,49 @@ package savt;
 /**
  * Created by enes on 07.05.2017.
  */
+<<<<<<< HEAD
 public class Personel {
 
 
 
+=======
+
+public abstract class Personel {
+
+    private String ID;
+    private int workHour;
+    private double maasCarpani;
+
+    public Personel(String ID)
+    {
+        this.ID = ID;
+        this.workHour = workHour;
+        this.maasCarpani = maasCarpani;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public int getWorkHour() {
+
+        return workHour;
+    }
+
+    public void setWorkHour(int workHour) {
+        this.workHour = workHour;
+    }
+
+    public double getMaasCarpani() {
+
+        return maasCarpani;
+    }
+    public void setMaasCarpani(double maasCarpani) {
+        this.maasCarpani = maasCarpani;
+    }
+>>>>>>> 94131b9... class step1
 }
---
+++ src/savt/ReyonGorevlisi.java
@@ -0,0 +1,14 @@
+package savt;
+
+/**
+ * Created by enes on 07.05.2017.
+ */
+public class ReyonGorevlisi extends Personel
+{
+    public ReyonGorevlisi(String ID)
+    {
+        super(ID);
+        setWorkHour(10);
+        setMaasCarpani(1.5);
+    }
+}
