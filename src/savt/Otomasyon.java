72c47a33552b8f8ec04912fae3721ae859d7fc09
eneserkoc <enes96@gmail.com>
2017-05-16 13:15:07 +0300

    otomasyon duzenlendi

--- src/savt/Otomasyon.java
+++ src/savt/Otomasyon.java
@@ -1,24 +1,24 @@
 package savt;
 
 import java.io.IOException;
-
+import java.util.Scanner;
 /**
- * Created by ResulSerkan on 16.05.2017.
+ * Created by enes on 16.05.2017.
  */
 @SuppressWarnings("resource")
 public class Otomasyon {
     public static void main(String[] args) {
 
-        Magaza magaza1 = new Magaza();
+        Magaza magaza = new Magaza("1201");
 
 
         try{
-            magaza1.urunleriOku();
+            magaza.urunleriOku();
         }
         catch( IOException e ) {
             System.out.println("Dosya okuma işlemleri sırasında" +
                     " bir hata oluştu.");
-            magaza1.yeniUrunTablosuOlustur();
+            magaza.yeniUrunTablosuOlustur();
             e.printStackTrace();
         }
         catch( ClassNotFoundException e ) {
@@ -27,6 +27,31 @@ public class Otomasyon {
             e.printStackTrace();
         }
 
+        Scanner in = new Scanner(System.in);
+        boolean exit = false;
+        System.out.println(" 1 - Yeni urun ekle");
+        System.out.println(" 2 - Kasa islemleri");
+        System.out.println(" 3 - Cikis");
+        System.out.print(" Seciminizi giriniz : ");
+        int selection = in.nextInt();
+        in.nextLine();
+
+        while(!exit)
+        {
+            switch(selection)
+            {
+                case 1 :    magaza.urunEkle();
+                break;
+
+                case 2 :    magaza.getKasa().Satis(magaza.getUrunler());
+                break;
+
+                case 3 :    System.out.println("Programdan cikis yaptiniz");
+                            exit = true;
+                break;
+                default : System.out.println("Hatali bir secim girdiniz...");
+            }
+        }
 
     }
 }
